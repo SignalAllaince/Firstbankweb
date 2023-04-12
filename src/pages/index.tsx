389dfc1da770ebd0794e668ebbf0d01ad4125a50
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-04-12 03:16:16 +0100

    feat: created custom switch component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import Modal from "@/components/modal";
+import CustomSwitch from "@/components/switch";
 import { Inter } from "next/font/google";
 import { useState } from "react";
 
@@ -12,19 +12,15 @@ export default function Home() {
     <main
       className={`${inter.className} flex min-h-screen flex-col items-center`}
     >
-      <div className="">
+      <div className="space-y-4">
         <h1 className="text-2xl">FirstBank Shop</h1>
         <button
           onClick={() => setOpen(true)}
-          className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+          className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-2 font-sans text-sm font-medium text-white shadow-sm hover:bg-indigo-700"
         >
           Open modal
         </button>
-        <Modal
-          isOpen={isOpen}
-          closeModal={setClose}
-          title="Payment successful"
-        ></Modal>
+        <CustomSwitch />
       </div>
     </main>
   );
