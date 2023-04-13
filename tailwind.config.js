96d9f9ad440574c22b6f04563844dc35359c5a62
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-04-13 16:30:24 +0100

    style: extended tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,20 @@ module.exports = {
       gridTemplateRows: {
         "[auto,auto,1fr]": "auto auto 1fr",
       },
+      colors: {
+        brand: {
+          50: '#fdf8f6',
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+        },
+      }
     },
   },
   plugins: [require("@tailwindcss/aspect-ratio")],
