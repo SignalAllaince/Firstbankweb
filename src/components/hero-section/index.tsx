e8495094659c3dcae01468d792b6071336741ee7
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-06-04 17:01:41 +0100

    fix: changed the banner background

--- src/components/hero-section/index.tsx
+++ src/components/hero-section/index.tsx
@@ -1,11 +1,12 @@
-import Heading from "../heading";
-
 function HeroSection() {
   return (
-    <div className="custom-hero flex h-[160px] w-full items-center justify-center">
-      <Heading size="h2" className="text-white">
-        FirstBank Brandshop
-      </Heading>
+    <div className="custom-hero rounded-0 mx-16 my-4 flex h-[244px] flex-col items-start justify-center pl-20 text-black">
+      {/* <Heading size="h1">Flash Sale</Heading>
+      <p className="max-w-[450px] text-sm">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam,
+        earum voluptatibus natus eveniet nulla quis incidunt quibusdam
+        blanditiis veniam vero eos repellendus?
+      </p> */}
     </div>
   );
 }
