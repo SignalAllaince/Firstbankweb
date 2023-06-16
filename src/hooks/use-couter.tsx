931bf1e954712b7e9e0fd453dd307d8f2ca9290b
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-06-16 11:06:41 +0100

    chore: added sync for hook

--- src/hooks/use-couter.tsx
+++ src/hooks/use-couter.tsx
@@ -3,9 +3,10 @@ import React from "react";
 function useCounter(limit = 10, start = 1) {
   const [quantity, setQuantity] = React.useState(start);
 
-  // React.useEffect(() => {
-  //   setQuantity(quantity)
-  // }, [start])
+  React.useEffect(() => {
+    setQuantity(quantity);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [start]);
 
   const increaseQuantity = () => setQuantity((prev) => Math.min(++prev, limit));
   const decreaseQuantity = () => quantity > 1 && setQuantity((prev) => --prev);
