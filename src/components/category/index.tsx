4a2846fc61b03a4dbd73b1021eab8c0a83d5a313
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-08-04 01:04:50 +0100

    feat: implemeted new ui fix

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,6 +1,8 @@
 import useGetCategoryProducts from "@/hooks/category/useGetCategoryProducts";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import { AnimatePresence } from "framer-motion";
+import Image from "next/image";
+import shirtImg from "../../../public/images/top.svg";
 import Button from "../button";
 import FadeInOut from "../fade";
 import Heading from "../heading";
@@ -8,6 +10,7 @@ import Icon from "../icon";
 import IfElse from "../if-else";
 import ProductCard from "../product-card";
 import ProductCardSkeleton from "../product-card/skeleton";
+import Section from "../section";
 
 function Catergory({
   header,
@@ -27,17 +30,22 @@ function Catergory({
     categoryId,
   });
 
+  const showMore =
+    noMore ||
+    (categoryProducts?.value
+      ? categoryProducts?.value?.products.length === 0
+      : false);
   return (
     <div className="space-y-6 py-10">
       <div
-        className={`flex w-full items-center justify-between px-2 py-2 ${
+        className={`flex h-14 w-full items-center justify-between px-2 py-2 ${
           indep ? "bg-[#B6D9A6]" : "bg-[#FFE18C]"
         }`}
       >
         <Heading size="h5" className="font-medium">
           {header}
         </Heading>
-        {!noMore && (
+        {!showMore && (
           <Button
             href={`/${slug}`}
             variant="cart"
@@ -61,19 +69,42 @@ function Catergory({
             </FadeInOut>
           }
         >
-          <FadeInOut className="product-grid grid gap-x-3 gap-y-10">
-            {categoryProducts?.value?.products.map((product) => (
-              <ProductCard
-                name={product.name}
-                key={product.id}
-                href={`/${product.slug}`}
-                // isFinished={product.stockQuantity === 0}
-                stockQuantity={product.stockQuantity}
-                price={product.calculatedProductPrice.priceString}
-                imageAlt={`${product.name} image`}
-              />
-            ))}
-          </FadeInOut>
+          <AnimatePresence>
+            <IfElse
+              ifOn={categoryProducts?.value?.products.length !== 0}
+              ifOnElse={categoryProducts?.value?.products.length === 0}
+              onElse={
+                <FadeInOut>
+                  <Section className="flex w-full flex-col items-center justify-center space-y-2">
+                    <div className="max-w-lg">
+                      <Image src={shirtImg} alt={"djsdsd"} />
+                    </div>
+                    <div className="flex flex-col items-center space-y-3 text-center">
+                      <Heading size="h3">No products found</Heading>
+                      <p className="text-sm">
+                        Try searching for more general terms or shop from other
+                        categories above.
+                      </p>
+                    </div>
+                  </Section>
+                </FadeInOut>
+              }
+            >
+              <FadeInOut className="product-grid grid gap-x-3 gap-y-10">
+                {categoryProducts?.value?.products.map((product) => (
+                  <ProductCard
+                    name={product.name}
+                    key={product.id}
+                    href={`/${product.slug}`}
+                    // isFinished={product.stockQuantity === 0}
+                    stockQuantity={product.stockQuantity}
+                    price={product.calculatedProductPrice.priceString}
+                    imageAlt={`${product.name} image`}
+                  />
+                ))}
+              </FadeInOut>
+            </IfElse>
+          </AnimatePresence>
         </IfElse>
       </AnimatePresence>
     </div>
---
+++ src/components/modal/logout/index.tsx
@@ -0,0 +1,36 @@
+import Button from "@/components/button";
+import { signOut } from "next-auth/react";
+import Modal from "..";
+
+function LogoutModal({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  return (
+    <Modal isOpen={isOpen} closeModal={onClose}>
+      <div className="space-y-8 py-6 md:px-5">
+        <div className="text-md mx-auto max-w-sm text-center font-light">
+          <p>Are you sure you want to Log out?</p>
+        </div>
+        <div className="flex flex-col items-center gap-3 sm:flex-row">
+          <Button
+            spinnerColor="#003B65"
+            onClick={() => signOut()}
+            className="w-full px-2 text-sm uppercase"
+            variant="secondary"
+          >
+            yes, logout
+          </Button>
+          <Button className="w-full px-2 text-sm uppercase" onClick={onClose}>
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default LogoutModal;
